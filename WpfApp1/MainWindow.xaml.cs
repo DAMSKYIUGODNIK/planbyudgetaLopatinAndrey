27bb2e3354bb3601d60a4a40d2f439b41f919720
unknown <you@example.com>
2023-11-22 13:52:57 +0700

    first commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodOrderingSystem
+
+ string item = $"Категория: {expense.Category}, Сумма: {expense.Amount}";           
+statsListBox.Items.Add(item);      
+
+
+public class Expense
+{
+    public double Amount { get; set; }
+    public string Category { get; set; }
+    public Expense(double amount, string category)
+    {
+        Amount = amount; Category = category;
+    }
+}
